7acebc04ffa1fd046c2fb77b0a9912cf6c163c83
vincentarthur <443519782@qq.com>
2016-09-02 22:59:38 +0800

    initial commit

---
+++ src/main/java/spring/boot/demo/entity/Entity.java
@@ -0,0 +1,23 @@
+//package spring.boot.demo.entity;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Entity {
+//
+//	@Value("${username}")
+//	private String username;
+//
+//	@Value("${password}")
+//	private String password;
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//}
